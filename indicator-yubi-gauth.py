a4c8d2fa369cd9fa001bf069fd1fb10b001d0ea2
damnshiok <damnshiok@gmail.com>
2014-01-23 04:00:54 -0600

    Update indicator-yubi-gauth.py

--- indicator-yubi-gauth.py
+++ indicator-yubi-gauth.py
@@ -1,42 +1,36 @@
 #!/usr/bin/env python
 
 ##################################################
-##
-##    Indicator for Yubikey Google Authenticator
-##
-##    by damnshiok@gmail.com
-##    with adapted code from Casey Link
-##
-##    Like this? Consider buying me a beer!
-##
-##    18mcyx2A78fUHjQwnrqca4H9N9XP6EqL5s
-##
+##						##
+##    Yubikey for Google Authenticator          ##
+##    by damnshiok 			        ##
+##						##
+##    Like this? Consider buying me a beer!     ##
+##      					##
+##    1GeGQciPbEPr2QoAJMZGGYNT2iYw1zKe4E	##
+##						##
 ##################################################
 
 import sys
 import gtk
 import appindicator
-import binascii
 import time
-import subprocess
 import struct
 import pynotify
+import yubico
 
-#icon from yubikey-personalization-gui (from yubico PPA)
-ICON = "/usr/share/pixmaps/yubikey-personalization-gui.png" 
-TIME_STEP = 30 # default as per TOTP spec
-TIME_CLEAR = 10 # number of seconds before clearing OTP from clipboard
+ICON = "/usr/share/pixmaps/yubikey-personalization-gui.png" #from yubikey-personalization-gui (yubico PPA)
+SLOT = 2
+STEP = 30
+DIGITS = 6
+TIME_CLEAR = 10 # number of seconds before clearing TOTP from clipboard 
 
-#check for python 3
-IS_PY3 = sys.version_info[0] == 3
+pynotify.init("yubi-totp") #initialize pynotify
 
-#initialize pynotify
-pynotify.init("yubi-gauth")
-
-class YubiIndicator:
+class yubikey_totp_indicator:
 
     def __init__(self):
-        self.ind = appindicator.Indicator("yubi-gauth", ICON, appindicator.CATEGORY_APPLICATION_STATUS)
+        self.ind = appindicator.Indicator("yubi-totp", ICON, appindicator.CATEGORY_APPLICATION_STATUS)
         self.ind.set_status(appindicator.STATUS_ACTIVE)
         self.menu_setup()
         self.ind.set_menu(self.menu)
@@ -45,18 +39,18 @@ class YubiIndicator:
     def menu_setup(self):
         self.menu = gtk.Menu()
 
-        self.yubi_item = gtk.MenuItem("Get OTP")
-        self.yubi_item.connect("activate", self.YUBI)
-        self.yubi_item.show()
-        self.menu.append(self.yubi_item)
+        self.get_totp_item = gtk.MenuItem("Get TOTP")
+        self.get_totp_item.connect("activate", self.get_totp)
+        self.get_totp_item.show()
+        self.menu.append(self.get_totp_item)
 
         self.quit_item = gtk.MenuItem("Quit")
         self.quit_item.connect("activate", self.quit)
         self.quit_item.show()
         self.menu.append(self.quit_item)
 
-    def YUBI(self, widget):		
-        yubi()
+    def get_totp(self, widget):		
+        make_totp()
 
     def main(self):
         gtk.main()
@@ -65,63 +59,49 @@ class YubiIndicator:
         gtk.main_quit()
         sys.exit(0)
 
-#next 3 functions are adapted from Casey Link
-def mangle_hash(h):
-    if IS_PY3:
-        offset = h[-1] & 0x0F
-    else:
-        offset = ord(h[-1]) & 0x0F
-    truncated_hash = h[offset:offset+4]
-   
-    code = struct.unpack(">L", truncated_hash)[0]
-    code &= 0x7FFFFFFF;
-    code %= 1000000;
-    
-    return '{0:06d}'.format(code)
-    
-def generate_challenges():
-    challenges = []
-    t = int(time.time())
-    tm = t/TIME_STEP
-    tm = struct.pack('>q', int(tm))
-    challenges.append(tm)
-    return challenges
- 
-def yubi():
-    print("Challenge sent to Yubikey")
-    n_challenge = pynotify.Notification("Yubikey for Google Authenticator", "Challenge sent to Yubikey", ICON)
-    n_challenge.set_timeout(500) #500ms
-    n_challenge.show()
-    for chal in generate_challenges():
-        chal = binascii.hexlify(chal)
-        cmd = ['ykchalresp']
-        cmd.append('-2x')
-        cmd.append(chal)
-        if hasattr(subprocess, "check_output"):
-            resp = subprocess.check_output(cmd).strip()
-        else:
-            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            out, err = proc.communicate()
-            if not isinstance(out, basestring):
-                raise ValueError("Command {0} returned {1!r}."
-                                 .format(" ".join(cmd), out))
-            resp = out.strip()
-        clipboard = gtk.clipboard_get()
-        clipboard.set_text("%s" %(mangle_hash(binascii.unhexlify(resp))))
-        clipboard.store()
-        print("OTP pasted to clipboard")
-        n_received = pynotify.Notification("Yubikey for Google Authenticator", "OTP pasted to clipboard", ICON)
-        n_received.show()
-        gtk.timeout_add(TIME_CLEAR * 1000, clear_otp)
+def make_totp():   # adapted from Yubico
 
-def clear_otp():
+    try:
+        YK = yubico.find_yubikey()
+        print YK
+        # Do challenge-response
+        current_time = int(time.time())
+        challenge = struct.pack("> Q", current_time / STEP).ljust(64, chr(0x0))
+        print "Sending challenge : %s" % (challenge.encode('hex'))
+	print "Press button if you configured your Yubikey Slot 2 to require user input."
+        response = YK.challenge_response(challenge, slot=SLOT)
+        print "Response received : %s" % (response.encode('hex'))
+        # format with appropriate number of leading zeros
+        fmt = "%." + str(DIGITS) + "i"
+        totp_str = fmt % (yubico.yubico_util.hotp_truncate(response, length=DIGITS))
+        print "Response formatted and truncated to %d digits: %s" % (DIGITS, totp_str)
+        write_to_clipboard(totp_str)
+        gtk.timeout_add(TIME_CLEAR * 1000, clear_totp)
+
+    except yubico.yubico_exception.YubicoError, e:
+        print "ERROR: %s" % (e.reason)
+        notify_error = pynotify.Notification("Yubikey for Google Authenticator","ERROR: %s" % (e.reason), ICON)
+        notify_error.show()
+        return 1
+
+    return 0
+
+def write_to_clipboard(totp_str):
+    clipboard = gtk.clipboard_get()
+    clipboard.set_text(totp_str)
+    clipboard.store()
+    print "TOTP written to clipboard and will be cleared in %d seconds." % TIME_CLEAR
+    notify_totp_written = pynotify.Notification("Yubikey for Google Authenticator", "TOTP written to clipboard and will be cleared in %d seconds" % TIME_CLEAR, ICON)
+    notify_totp_written.show()
+
+def clear_totp():
     clipboard = gtk.clipboard_get()
     clipboard.set_text("")
     clipboard.store()
-    print("OTP cleared from clipboard")
-    n_cleared = pynotify.Notification("Yubikey for Google Authenticator", "OTP cleared from clipboard", ICON)
-    n_cleared.show()
+    print "TOTP cleared from clipboard"
+    notify_totp_cleared = pynotify.Notification("Yubikey for Google Authenticator", "TOTP cleared from clipboard", ICON)
+    notify_totp_cleared.show()
 
 if __name__ == "__main__":
-    indicator = YubiIndicator()
+    indicator = yubikey_totp_indicator()
     indicator.main()
